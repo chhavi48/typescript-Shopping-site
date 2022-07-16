5c22eaac1fff5810ca725b724c6c57819d7b22da
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-07-16 19:44:04 +0530

    typescript

---
+++ src/pages/Store.tsx
@@ -0,0 +1,18 @@
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+import storeItems from "../data/items.json"
+
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row  className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
